25e90e7bcf123b97cd17a13c9abc36ebaaa82e57
ParkerHadley <parker.l.hadley@gmail.com>
2022-02-18 21:20:43 -0500

    Write basic application generation function and initiate destination

--- app.js
+++ app.js
@@ -4,3 +4,17 @@
 // Project:         Random Trip Generator
 
 "use strict"
+
+//Writing generator function//
+    function randomTripGenerator (){
+    console.log("Are you ready to take a trip?");
+
+// Randomly and repeatedly select a destination from the array.//
+let destinations = ["College", "Ridgeway", "Gateway", "Ester", "Sitka", "Soldotna", "Juneau", "Kenai", "Anchorage", "Palmer", "Wasilla"];
+
+let randomDestinations = destinations[Math.floor(Math.random() * destinations.length)];{
+    console.log(randomDestinations, destinations[randomDestinations]);
+}
+    }
+
+    randomTripGenerator();
